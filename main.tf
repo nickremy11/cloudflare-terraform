5aa3d2267c44e501ac518fb0be72ba4e82baf9fb
Nick Remy <nremy@cloudflare.com>
2026-06-02 15:53:08 -0400

    Step 3 - Enable SSL settings

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "cloudflare" {
- #API Token will be CLOUDFLARE_API_TOKEN env variable
+#AAA
 }
 
 variable "zone_id" {
@@ -29,12 +29,23 @@ variable "domain" {
   default = "remydemo.com"
 }
 
-resource "cloudflare_dns_record" "aaa" {
-  zone_id = "var.zone_id"
-  name    = "a"
-  content = "200.100.11.13"
-  type    = "A"
-  ttl     = 1
-  proxied = true
-  comment = "Domain verification record"
+# Enable TLS 1.3
+resource "cloudflare_zone_setting" "tls_1_3" {
+  zone_id    = var.zone_id
+  setting_id = "tls_1_3"
+  value      = "on"
+}
+
+# Enable automatic HTTPS rewrites
+resource "cloudflare_zone_setting" "automatic_https_rewrites" {
+  zone_id    = var.zone_id
+  setting_id = "automatic_https_rewrites"
+  value      = "on"
+}
+
+# Set SSL mode to strict
+resource "cloudflare_zone_setting" "ssl" {
+  zone_id    = var.zone_id
+  setting_id = "ssl"
+  value      = "strict"
 }
