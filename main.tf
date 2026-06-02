d7427c390781aa381c2b38848a17633e24b1cc43
Nick Remy <nremy@cloudflare.com>
2026-06-02 15:56:30 -0400

    Step 3.1 - Add WAF Rule

--- main.tf
+++ main.tf
@@ -49,3 +49,18 @@ resource "cloudflare_zone_setting" "ssl" {
   setting_id = "ssl"
   value      = "strict"
 }
+
+resource "cloudflare_ruleset" "zone_custom_firewall" {
+  zone_id     = var.zone_id
+  name        = "Phase entry point ruleset for custom rules in my zone"
+  description = ""
+  kind        = "zone"
+  phase       = "http_request_firewall_custom"
+
+  rules = [{
+    ref         = "block_non_default_ports"
+    description = "Block ports other than 80 and 443"
+    expression  = "(not cf.edge.server_port in {80 443})"
+    action      = "block"
+  }]
+}
